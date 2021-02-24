fc8d67b6e6a3a819b32b5c73cf1de4b47d76ba12
João Boavida <j.m.boavida@gmail.com>
2021-02-24 12:10:50 -0300

    Initial Commit

---
+++ FlightRests/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlightRests
+//
+//  Created by Joao Boavida on 24/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlightRests/FlightRestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightRestsApp.swift
+//  FlightRests
+//
+//  Created by Joao Boavida on 24/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct FlightRestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
