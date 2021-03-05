d72202f2420de8eaab25d16e450810393098f087
João Boavida <j.m.boavida@gmail.com>
2021-03-05 06:46:18 -0300

    Assigned rest period owner is now an int.

--- FlightRests/AssignedRestPeriod.swift
+++ FlightRests/AssignedRestPeriod.swift
@@ -9,10 +9,10 @@ import Foundation
 
 struct AssignedRestPeriod: Identifiable {
     let id = UUID()
-    let owner: String
+    let owner: Int
     let period: DateInterval
 
-    static let example1 = AssignedRestPeriod(owner: "Pilot 1", period: DateInterval(start: DateComponents(calendar: .autoupdatingCurrent, timeZone: .autoupdatingCurrent, year: 2021, month: 2, day: 19, hour: 15).date!, duration: 7200))
+    static let example1 = AssignedRestPeriod(owner: 1, period: DateInterval(start: DateComponents(calendar: .autoupdatingCurrent, timeZone: .autoupdatingCurrent, year: 2021, month: 2, day: 19, hour: 15).date!, duration: 7200))
 
-    static let example2 = AssignedRestPeriod(owner: "Pilot 2", period: DateInterval(start: DateComponents(calendar: .autoupdatingCurrent, timeZone: .autoupdatingCurrent, year: 2021, month: 2, day: 10, hour: 04, minute: 30).date!, duration: 9600))
+    static let example2 = AssignedRestPeriod(owner: 2, period: DateInterval(start: DateComponents(calendar: .autoupdatingCurrent, timeZone: .autoupdatingCurrent, year: 2021, month: 2, day: 10, hour: 04, minute: 30).date!, duration: 9600))
 }
