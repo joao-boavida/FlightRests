7aeee059bacd2f4a0e5e0e4456c0b407d6754256
João Boavida <j.m.boavida@gmail.com>
2021-07-21 12:38:06 +0100

    Test implemented for the distribution of rest units

--- FlightRestsTests/FlightRestsTests.swift
+++ FlightRestsTests/FlightRestsTests.swift
@@ -18,16 +18,64 @@ class FlightRestsTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    /// checks a given distribution of rest units is fair and optimal
+    /// - Parameters:
+    ///   - result: the results to be analysed
+    ///   - numberOfPeriods: number of rest periods
+    ///   - totalUnits: number of total rest units
+    ///   - numberOfUsers: number of users
+    fileprivate func checkDistributedRestUnits(_ result: [Int], _ numberOfPeriods: Int, _ totalUnits: Int, _ numberOfUsers: Int) {
+        let totalResultUnits = result.reduce(0, +)
+        let numberOfBreaks = numberOfPeriods - 1
+
+        let unusedUnits = totalUnits - totalResultUnits
+
+        // the number of unused units must be less thant the number of breaks, otherwise further break optimisation is possible
+        XCTAssertLessThan(unusedUnits, numberOfBreaks, "There are unused units which could be used to increment rests or breaks, \(numberOfUsers) users,  \(numberOfPeriods) rest periods")
+
+        let restUnits = result.enumerated().compactMap {
+            $0.offset % 2 == 0 ? $0.element : nil
+        }
+
+        XCTAssertTrue(numberOfUsers == 2 || numberOfUsers == 3, "Unsupported number of users")
+
+        if numberOfUsers == 2 {
+            // check for fair distribution 2 users
+            let user1 = restUnits.enumerated().compactMap {
+                $0.offset % 2 == 0 ? $0.element : nil
+            }
+            let totalUser1 = user1.reduce(0, +)
+            let user2 = restUnits.enumerated().compactMap {
+                $0.offset % 2 == 1 ? $0.element : nil
+            }
+            let totalUser2 = user2.reduce(0, +)
+            XCTAssertEqual(totalUser1, totalUser2, "Unfair distribution of rest periods, \(numberOfUsers) users,  \(numberOfPeriods) rest periods")
+        } else {
+            // 3 rest periods for sure
+            XCTAssertEqual(restUnits[0], restUnits[1], "Unfair distribution of rest periods, \(numberOfUsers) users,  \(numberOfPeriods) rest periods")
+            XCTAssertEqual(restUnits[0], restUnits[2], "Unfair distribution of rest periods, \(numberOfUsers) users,  \(numberOfPeriods) rest periods")
+        }
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    /// Tests if the distribution of rest units is being fair and optimised for several user/period cases.
+    func testDistributeRestUnits() throws {
+        let minimumBreakUnits = 1
+        let totalUnits = 30
+
+        var numberOfUsers = 2
+
+        for numberOfPeriods in 2 ... 4 {
+            let result = RestCalculator.distributeRestPlanUnits(numberOfUsers: numberOfUsers, numberOfPeriods: numberOfPeriods, minimumBreakUnits: minimumBreakUnits, totalUnits: totalUnits)
+
+            checkDistributedRestUnits(result, numberOfPeriods, totalUnits, numberOfUsers)
         }
+
+        numberOfUsers = 3
+
+        let result = RestCalculator.distributeRestPlanUnits(numberOfUsers: numberOfUsers, numberOfPeriods: 3, minimumBreakUnits: minimumBreakUnits, totalUnits: totalUnits)
+
+        checkDistributedRestUnits(result, 3, totalUnits, numberOfUsers)
+
     }
 
 }
