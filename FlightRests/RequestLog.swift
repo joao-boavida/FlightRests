2a711272976d8a8822614ca65f15dd8a403cb2e5
João Boavida <j.m.boavida@gmail.com>
2021-11-13 18:01:11 +0100

    The rest requests are now stored in an array that checks for true duplicates before storage.

--- FlightRests/RequestLog.swift
+++ FlightRests/RequestLog.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 final class RequestLog: ObservableObject {
-    @Published private(set) var requests: Set<RestRequest>
+    @Published private(set) var requests: [RestRequest]
 
     static let saveKey = "RequestLog"
 
     init() {
-        let result = FileManager.loadFromDocumentsFolder(Set<RestRequest>.self, fileName: Self.saveKey)
+        let result = FileManager.loadFromDocumentsFolder([RestRequest].self, fileName: Self.saveKey)
 
         switch result {
         case .success(let requests):
@@ -38,14 +38,24 @@ final class RequestLog: ObservableObject {
     /// Adds an element to the requests array, then saves
     /// - Parameter request: element to add
     func addRequest(_ request: RestRequest) {
-        requests.insert(request)
+        // check if any true duplicate (an element equal to a member of the array except for creation date) exists
+        let isTrueDuplicate = requests.map {
+            request.isTrueDuplicateOf($0)
+        }.contains(true)
+
+        guard isTrueDuplicate == false else { return } // in this case do not add the item
+
+        // the item is not a true duplicate, then it should be added
+        requests.append(request)
         save()
     }
 
     /// Removes an element from the requests array, then saves
     /// - Parameter element: element to remove
     func removeRequest(_ element: RestRequest) {
-        requests.remove(element)
+        if let index = requests.firstIndex(of: element) {
+            requests.remove(at: index)
+        }
         save()
     }
 }
