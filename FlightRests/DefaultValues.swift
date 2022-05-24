5be5901740d2793ff8ef285b72d2def8c9795f9a
João Boavida <j.m.boavida@gmail.com>
2022-05-24 16:10:08 -0300

    new file

---
+++ FlightRests/DefaultValues.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultValues.swift
+//  FlightRests
+//
+//  Created by João Boavida on 24/05/2022.
+//
+
+import Foundation
+
+/// Stores default selections in the Input Screen;
+struct DefaultValues {
+    static let users = 2
+    static let restPeriods = 2
+    static let minimumBreakSelection = 1
+    static let optimiseBreaks = false
+    static let beforeServiceSelection = 2
+    static let serviceSelection = 4
+}
