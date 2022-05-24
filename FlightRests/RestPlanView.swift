149ef7ec317226c45ae6c50b2496317b795f52c5
João Boavida <j.m.boavida@gmail.com>
2022-05-24 17:09:43 -0300

    added content centering when the vertical size is not constrained

--- FlightRests/RestPlanView.swift
+++ FlightRests/RestPlanView.swift
@@ -26,23 +26,45 @@ struct RestPlanView: View {
     }
 
     @Environment(\.timeZone) var environmentTimeZone
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+
     let timeColors = [Color.blue, Color.red, Color.green, Color.purple, Color.orange]
 
     var body: some View {
-        ScrollView {
-            VStack {
-                if restPlan.isEmpty {
-                    Text("No Data to Display")
-                        .font(.title)
-                } else {
+        if restPlan.isEmpty {
+            Text("No Data to Display")
+                .font(.title)
+                .navigationBarTitle(titleString)
+        } else {
+            if verticalSizeClass == .compact {
+                ScrollView {
+                    VStack {
+                        // ternary operator added here as a safeguard to prevent crash due to index out of range; the color black should never be used.
+                        ForEach(restPlan) { period in
+                            RestPeriodView(restPeriod: period, timeColour: period.owner <= timeColors.count ? timeColors[period.owner - 1] : Color.black).environment(\.timeZone, environmentTimeZone)
+                        }.padding(.vertical)
+                    }.navigationBarTitle(titleString)
+
+                }
+            } else {
+                VStack {
+                    Text(titleString)
+                        .font(.largeTitle)
+                    Spacer()
+                        .frame(maxHeight: 50)
                     // ternary operator added here as a safeguard to prevent crash due to index out of range; the color black should never be used.
                     ForEach(restPlan) { period in
                         RestPeriodView(restPeriod: period, timeColour: period.owner <= timeColors.count ? timeColors[period.owner - 1] : Color.black).environment(\.timeZone, environmentTimeZone)
                     }.padding(.vertical)
+                    Spacer()
+                        .frame(maxHeight: 50)
+                    Text("🛩 🌙 🛩")
+                        .font(.largeTitle)
                 }
+
             }
         }
-        .navigationBarTitle(titleString)
+
     }
 }
 
